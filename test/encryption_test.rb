f7d6a0540b58a743411cc02c9f0d279a85c53342
Nick King <nickmaxking@gmail.com>
2020-09-18 10:41:58 -0600

    Add tests for initialize and readable attributes

---
+++ test/encryption_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/encryption'
+
+class EncryptionTest < Minitest::Test
+  def test_it_exists
+    encryption = Encryption.new("Hello World", "12345", "050420")
+
+    assert_instance_of Encryption, encryption
+  end
+
+  def test_it_has_readable_attributes
+    encryption = Encryption.new("Hello World", "12345", "050420")
+
+    assert_equal "Hello World", encrytion.message
+    assert_equal "12345", encryption.key
+    assert_equal "050420", encrytion.date
+  end
+end
