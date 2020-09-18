236bce5a69332912f0ed46eded004233bebcf64c
Nick King <nickmaxking@gmail.com>
2020-09-18 11:55:19 -0600

    Add mdefault key and date functionality with key and date method. Remove attr_readers for key and date

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,10 +1,25 @@
 class Encryption
-  attr_reader :message,
-              :key,
-              :date
-  def initialize(message, key, date)
+  attr_reader :message
+
+  def initialize(message, key = nil, date = Date.today)
     @message = message
     @key = key
     @date = date
+  end
+
+# Move to module?
+  def key
+    if @key == nil
+      @key = Kernel.rand(99999).to_s.rjust(5, "0")
+    end
+    @key
+  end
+
+# Move to module?
+  def date
+    if @date.class == Date
+      @date = @date.strftime("%d%m%y")
+    end
+    @date
   end
 end
