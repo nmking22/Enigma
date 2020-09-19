be619374f116fdf6b6e4ecc04e481016dc2a33e5
Nick King <nickmaxking@gmail.com>
2020-09-19 01:35:31 -0600

    Add attr_readers

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -2,7 +2,12 @@ require './lib/shiftable'
 
 class Decryption
   include Shiftable
+  attr_reader :ciphertext,
+              :key,
+              :date
   def initialize(ciphertext, key, date)
-
+    @ciphertext = ciphertext
+    @key = key
+    @date = date
   end
 end
