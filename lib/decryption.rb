35b7ed5ced1373e469712ebae32fcc6f4bb67996
Nick King <nickmaxking@gmail.com>
2020-09-19 02:16:01 -0600

    Add shift, shift_into_character_set methods. Add character set attr_reader and instance variable

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -3,12 +3,30 @@ require './lib/shiftable'
 class Decryption
   include Shiftable
   attr_reader :ciphertext,
-              :key,
-              :offset
+              :key, # is this needed?
+              :offset,
+              :character_set
   def initialize(ciphertext, key, date = Date.today)
     @ciphertext = ciphertext
     @key = key
     @date = date
     @offset = create_offset
+    @character_set = ("a".."z").to_a << " "
+  end
+
+  def shift(letter, shift_type)
+    new_index = @character_set.index(letter) - shift_type
+    @character_set[shift_into_character_set(new_index)]
+  end
+
+  def shift_into_character_set(index)
+    loop do
+      if index < 0
+        index += 27
+      else
+        break
+      end
+    end
+    index
   end
 end
