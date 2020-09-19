159a0a0e95f6b3b57fd488f270cc0ecd683c901c
Nick King <nickmaxking@gmail.com>
2020-09-19 00:56:40 -0600

    Add test for encrypt. Remove unecessary requires. Add enigma require

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,14 +1,24 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/encryption'
+require './lib/enigma'
 require 'date'
-require 'mocha/minitest'
-require 'mocha/setup'
 
 class EnigmaTest < Minitest::Test
   def test_it_exists
     enigma = Enigma.new
 
     assert_instance_of Enigma, enigma
+  end
+
+  def test_it_can_encrypt
+    enigma = Enigma.new
+
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
   end
 end
