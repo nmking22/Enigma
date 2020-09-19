b5e19ac185def374b6f4192b090ffdea378839ce
Nick King <nickmaxking@gmail.com>
2020-09-19 01:45:43 -0600

    Add offset attr_reader and innstance variable in initialize

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -3,10 +3,12 @@ require './lib/shiftable'
 class Decryption
   include Shiftable
   attr_reader :ciphertext,
-              :key
+              :key,
+              :offset
   def initialize(ciphertext, key, date = Date.today)
     @ciphertext = ciphertext
     @key = key
     @date = date
+    @offset = create_offset
   end
 end
