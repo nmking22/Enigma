26c8ba35c52a0248c4007698ecda294ba94a3d37
Nick King <nickmaxking@gmail.com>
2020-09-19 01:31:49 -0600

    Change assert_equal to assert_instance_of

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -9,6 +9,6 @@ class DecryptionTest < Minitest::Test
   def test_it_exists
     decryption = Decryption.new("keder ohulw", "02715", "040895")
 
-    assert_equal Decryption, decryption
+    assert_instance_of Decryption, decryption
   end
 end
