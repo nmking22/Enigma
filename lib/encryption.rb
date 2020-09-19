716ae5cc379a9cbd094923f54dfbbd2204e72d17
Nick King <nickmaxking@gmail.com>
2020-09-19 01:21:22 -0600

    Move shared methods into shiftable module

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,4 +1,7 @@
+require './lib/shiftable'
+
 class Encryption
+  include Shiftable
   attr_reader :message,
               :character_set,
               :offset
@@ -11,80 +14,18 @@ class Encryption
     @offset = create_offset
   end
 
-# Move to module?
   def key
     if @key == nil
       @key = Kernel.rand(99999).to_s.rjust(5, "0")
     end
     @key
   end
-# Move to module?
+
   def date
     if @date.class == Date
       @date = @date.strftime("%d%m%y")
     end
     @date
-  end
-# Move to module?
-  def a_shift
-    @key[0..1].to_i + collect_offset_digit(0)
-  end
-  # Move to module?
-  def b_shift
-    @key[1..2].to_i + collect_offset_digit(1)
-  end
-  # Move to module?
-  def c_shift
-    @key[2..3].to_i + collect_offset_digit(2)
-  end
-  # Move to module?
-  def d_shift
-    @key[3..4].to_i + collect_offset_digit(3)
-  end
-
-  def create_offset
-    (date.to_i * date.to_i).to_s[-4..-1]
-  end
-
-  def collect_offset_digit(index)
-    @offset[index].to_i
-  end
-
-  def encrypt_message
-    shift_counter = 1
-    message_array = @message.split("")
-    encrypted_message = message_array.map do |letter|
-      if shift_counter % 4 == 1
-        shift_counter += 1
-        shift(letter, a_shift)
-      elsif shift_counter % 4 == 2
-        shift_counter += 1
-        shift(letter, b_shift)
-      elsif shift_counter % 4 == 3
-        shift_counter += 1
-        shift(letter, c_shift)
-      elsif shift_counter % 4 == 0
-        shift_counter += 1
-        shift(letter, d_shift)
-      end
-    end
-    encrypted_message.join
-  end
-
-  def shift(letter, shift_type)
-    new_index = @character_set.index(letter) + shift_type
-    @character_set[shift_into_character_set(new_index)]
-  end
-
-  def shift_into_character_set(index)
-    loop do
-      if index > 26
-        index -= 27
-      else
-        break
-      end
-    end
-    index
   end
 
   def encrypt
