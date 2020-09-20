4034f836877f872d830f04c5de2720a18ad5c30e
Nick King <nickmaxking@gmail.com>
2020-09-20 16:35:55 -0600

    Add high level notes for find_shifted_offset

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -11,4 +11,19 @@ class Cracker
     @offset = create_offset
     @character_set = ("a".."z").to_a << " "
   end
+
+  # find shifted offset to be used on last four chars
+  # check length of ciphertext % 4
+    # if 1 -> rotate 3
+    # if 2 -> rotate 2
+    # if 3 -> rotate 1
+    # if 4 -> return offset
+    # ROTATION PROCESS:
+      # convert offset to array
+      # use array.rotate(num)
+      # convert offset back to string
+
+  # pull out last four characters of string
+  # ending indexes: 26, 4, 13, 3
+
 end
