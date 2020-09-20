ecd3f9d4da108647ea6c88d4e9fe6f22bfff032b
Nick King <nickmaxking@gmail.com>
2020-09-19 23:47:48 -0600

    Add decrypt runner file functionality

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,1 +1,13 @@
+require './lib/enigma'
+require './lib/decryption'
+require 'date'
 
+file = File.open(ARGV[0], "r")
+
+message = file.read.delete("\n")
+
+enigma = Enigma.new
+decryption = enigma.decrypt(message, ARGV[2], ARGV[3])
+decryption_file = File.open(ARGV[1], "w")
+decryption_file.write(decryption[:decryption])
+puts "Created 'decrypted.txt' with the key #{decryption[:key]} and date #{decryption[:date]}"
