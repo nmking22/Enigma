0be864d550c6538cbf0d9aac3f50913f22d1dd78
Nick King <nickmaxking@gmail.com>
2020-09-20 13:35:04 -0600

    Create cracker_test and add tests for initialize and readable attributes

---
+++ test/cracker_test.rb
@@ -0,0 +1,23 @@
+require './test/test_helper'
+require './lib/cracker'
+require 'mocha/minitest'
+require 'mocha/setup'
+
+class CrackerTest < Minitest::Test
+  def test_it_exists
+    cracker = Cracker.new("vjqtbeaweqihssi", "291018")
+
+    assert_instance_of Cracker, cracker
+  end
+
+  def test_it_has_readable_attributes
+    cracker = Cracker.new("vjqtbeaweqihssi", "291018")
+
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+      "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal "vjqtbeaweqihssi", cracker.ciphertext
+    assert_equal "291018", cracker.key
+    assert_equal expected, cracker.character_set
+  end
+end
