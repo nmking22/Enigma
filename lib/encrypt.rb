e46bf588fc6a44b1295d6dbe9e8bb5ad975f2643
Nick King <nickmaxking@gmail.com>
2020-09-19 23:27:22 -0600

    Add functioning encrypt.rb runner

---
+++ lib/encrypt.rb
@@ -0,0 +1,12 @@
+require './lib/enigma'
+require './lib/encryption'
+require 'date'
+
+file = File.open(ARGV[0], "r")
+
+message = file.read.delete("\n")
+
+enigma = Enigma.new
+encryption = enigma.encrypt(message)
+encryption_file = File.open(ARGV[1], "w")
+encryption_file.write(encryption[:encryption])
