6855e2cafe0caacacbe8f24837bda729d14488c3
Nick King <nickmaxking@gmail.com>
2020-09-20 16:37:49 -0600

    Add conditional logic for find_shifted_offset

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -12,6 +12,13 @@ class Cracker
     @character_set = ("a".."z").to_a << " "
   end
 
+  def find_shifted_offset
+    if @ciphertext.length % 4 == 1
+    elsif @ciphertext.length % 4 == 2
+    elsif @ciphertext.length % 4 == 3
+    elsif @ciphertext.length % 4 == 0
+    end
+  end
   # find shifted offset to be used on last four chars
   # check length of ciphertext % 4
     # if 1 -> rotate 3
