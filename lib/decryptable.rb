b519d40f57905c9770830ee0b29df2b0ab726912
Nick King <nickmaxking@gmail.com>
2020-09-21 10:51:11 -0600

    Add shift and shift_into_character set methods

--- lib/decryptable.rb
+++ lib/decryptable.rb
@@ -1,3 +1,21 @@
 module Decryptable
+  def shift(letter, shift_type)
+    if @character_set.include? letter
+      new_index = @character_set.index(letter) - shift_type
+      @character_set[shift_into_character_set(new_index)]
+    else
+      letter
+    end
+  end
 
+  def shift_into_character_set(index)
+    loop do
+      if index < 0
+        index += 27
+      else
+        break
+      end
+    end
+    index
+  end
 end
