bb51c5015f5bfef88a05676274f942e845b5e886
Nick King <nickmaxking@gmail.com>
2020-09-20 19:12:14 -0600

    Add ending_indexes attr_reader test and change last_four_character expected to array

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -5,13 +5,15 @@ class Cracker
   attr_reader :ciphertext,
               :offset,
               :character_set,
-              :last_four_characters
+              :last_four_characters,
+              :ending_indexes
   def initialize(ciphertext, date = Date.today)
     @ciphertext = ciphertext
     @date = date
     @offset = create_offset
     @character_set = ("a".."z").to_a << " "
-    @last_four_characters = ciphertext[-4..-1]
+    @last_four_characters = ciphertext[-4..-1].split("")
+    @ending_indexes = [26, 4, 14, 3]
   end
 
   def shifted_offset
@@ -30,20 +32,20 @@ class Cracker
     new_offset = @offset.split("")
     new_offset.rotate(number).join
   end
-  # find shifted offset to be used on last four chars
-  # check length of ciphertext % 4
-    # if 1 -> rotate 3
-    # if 2 -> rotate 2
-    # if 3 -> rotate 1
-    # if 4 -> return offset
-    # ROTATION PROCESS:
-      # convert offset to array
-      # use array.rotate(num)
-      # convert offset back to string
 
+  def find_shifts
+    # helper method to array and rotate offsets
+    # iterate through last_four_characters
+    # require 'pry'; binding.pry
+    shifts_array = []
+    @last_four_characters.each_with_index do |letter, index|
+      shifts_array << (@character_set.index(letter) - ending_indexes[index])
+    end
+    shifts_array
+  end
   # pull out last four characters of string
   # ending final indexes: 26, 4, 13, 3
   # ending test indexes: 7, 18, 18, 8
   # shifts: 8, 14, 5, 5
-
+  # char_index - ending_index = shift
 end
