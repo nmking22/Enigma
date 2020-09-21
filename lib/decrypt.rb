6cefbefbe7a40f24c5f51073cbe4bcf8753fa702
Nick King <nickmaxking@gmail.com>
2020-09-21 11:06:51 -0600

    Change hard coded .txt file name in output to string interpolation

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -10,4 +10,4 @@ enigma = Enigma.new
 decryption = enigma.decrypt(message, ARGV[2], ARGV[3])
 decryption_file = File.open(ARGV[1], "w")
 decryption_file.write(decryption[:decryption])
-puts "Created 'decrypted.txt' with the key #{decryption[:key]} and date #{decryption[:date]}"
+puts "Created '#{ARGV[1]}' with the key #{decryption[:key]} and date #{decryption[:date]}"
